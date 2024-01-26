e24c9bb40a3516a792dfaa945ced0ec9ce1ac671
Bruce <irabruce10@gmail.com>
2024-01-26 11:27:47 +0100

    Initialize project using create React app

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import Service from "./Service";
 import { useState } from "react";
 
 function App() {
-  const [bill, setBill] = useState(0);
+  const [bill, setBill] = useState("");
   const [parc, setParc] = useState(0);
   const [parc1, setParc1] = useState(0);
 
   const sum = bill * [(parc + parc1) / 2 / 100];
+
+  function handleReset() {
+    setBill("");
+    setParc(0);
+    setParc1(0);
+  }
 
   return (
     <div>
@@ -22,7 +28,9 @@ function App() {
         how did your friend like the Service?
       </Service>
 
-      <Result bill={bill} parc={parc} sum={sum} />
+      {bill > 0 && (
+        <Result bill={bill} parc={parc} sum={sum} handleReset={handleReset} />
+      )}
     </div>
   );
 }
--- src/Result.js
+++ src/Result.js
@@ -1,11 +1,11 @@
-export default function Result({ bill, onBill, parc, handleBill, sum, onSum }) {
+export default function Result({ bill, parc, handleReset, sum }) {
   return (
     <div>
       <h1>
         you pay €{bill + sum} (€{bill}+€{sum})
       </h1>
 
-      <button>Reset</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
